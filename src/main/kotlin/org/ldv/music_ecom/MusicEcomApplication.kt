be7cceaed5f41828cb0615077988d92213838f65
KendyCode <kendyprox@gmail.com>
2025-11-10 08:59:26 +0100

    first commit

---
+++ src/main/kotlin/org/ldv/music_ecom/MusicEcomApplication.kt
@@ -0,0 +1,11 @@
+package org.ldv.music_ecom
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MusicEcomApplication
+
+fun main(args: Array<String>) {
+    runApplication<MusicEcomApplication>(*args)
+}
---
+++ src/test/kotlin/org/ldv/music_ecom/MusicEcomApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ldv.music_ecom
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MusicEcomApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
